231d3ca308f069969a63eaaad0b548e6da9601cf
Francois <francois@happyhorse.co.za>
2011-09-14 22:41:10 +0200

    date time formatting helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def time_formatted(time)
+    time.strftime("%m/%d/%Y %I:%M%p (AZ)") rescue nil
+  end
+
+  def date_formatted(date)
+    date.strftime("%m/%d/%Y") rescue nil
+  end
 end
