9258d8801a70e1c85a064538bc386432b3671a15
Francois <francois@happyhorse.co.za>
2011-09-21 23:02:37 +0200

    fix date picker

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -44,7 +44,7 @@ var ajax_date = function(node) {
       var input = $('<input name="'+$(this).attr('id')+'" type="text"/>').attr('size', current.length + 1).val(current);
 
       input.datepicker({
-        dateFormat: 'yy-mm-dd',
+        dateFormat: 'mm-dd-yy',
         defaultDate: current,
         onClose : function(dateText, node) {
           if (dateText && dateText !== current) post_update($(this), update_notice);
