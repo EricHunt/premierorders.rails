2079e5160b660b99a9e9f40e1fec8e118291439e
Francois <francois@happyhorse.co.za>
2011-09-28 23:30:50 +0200

    another format change

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
   def time_formatted(time)
-    time.strftime("%m-%d-%Y %I:%M%p (AZ)") rescue nil
+    time.strftime("%m/%d/%Y %I:%M%p (AZ)") rescue nil
   end
 
   def date_formatted(date)
     #date.strftime("%m/%d/%Y") rescue nil
-    date.strftime("%Y-%m-%d") rescue nil
+    date.strftime("%m/%d/%Y") rescue nil
   end
 end
